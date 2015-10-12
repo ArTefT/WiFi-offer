e0ee83658d89a23e55d913c6879aa000e535c6a2
Aleksandr <arteft@ukr.net>
2015-10-12 12:19:35 +0300

    Update main.java

--- app/src/main/java/ua/arteft/wifioffer/main.java
+++ app/src/main/java/ua/arteft/wifioffer/main.java
@@ -21,23 +21,23 @@ public class main extends Activity implements OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-//made buttons
+//привязываем кнопки
         btnStart = (Button)findViewById(R.id.btnStart);
         btnStop = (Button) findViewById(R.id.btnStop);
-
+// даем слушателя
         btnStart.setOnClickListener(this);
         btnStop.setOnClickListener(this);
     }
 
-
+// указываем действие при срабатывании слушателя
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.btnStart:
-                startService(new Intent(this, services.class));
+                startService(new Intent(this, services.class));//запускаем сервис
                 break;
             case R.id.btnStop:
-               stopService(new Intent(this, services.class));
+               stopService(new Intent(this, services.class));//убиваем сервис
                 break;
         }
 
